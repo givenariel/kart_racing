35366c05c43fb16311f0489e713dc226363eb6a0
Fadhil <alfadhil636@gmail.com>
2025-03-19 20:54:44 +0700

    AH

---
+++ Assets/Digger/Modules/AdvancedOperations/Sources/ModificationJobs/SplineWalker/SplineWalker.cs
@@ -0,0 +1,44 @@
+﻿using Digger.Modules.AdvancedOperations.Splines;
+using Digger.Modules.Core.Sources;
+using Unity.Jobs;
+using UnityEngine;
+
+namespace Digger.Modules.AdvancedOperations.Sources.ModificationJobs.SplineWalker
+{
+    public class SplineWalker
+    {
+        private readonly DiggerSystem[] diggerSystems;
+
+        public delegate IOperation<T> OperationAt<T>(Vector3 position) where T : struct, IJobParallelFor;
+
+        public SplineWalker(DiggerSystem[] diggerSystems)
+        {
+            this.diggerSystems = diggerSystems;
+        }
+
+        public void WalkAlongSpline<T>(BezierSpline spline, float step, OperationAt<T> getOperationAt) where T : struct, IJobParallelFor
+        {
+            var length = spline.GetApproxLength();
+            step /= length;
+            for (var t = 0f; t < 1f; t += step) {
+                var operation = getOperationAt(spline.GetPoint(t));
+                DoOperation(operation);
+            }
+
+            foreach (var diggerSystem in diggerSystems) {
+                diggerSystem.BuildPendingMeshes();
+            }
+        }
+
+        private void DoOperation<T>(IOperation<T> operation) where T : struct, IJobParallelFor
+        {
+            foreach (var diggerSystem in diggerSystems) {
+                var area = operation.GetAreaToModify(diggerSystem);
+                if (!area.NeedsModification)
+                    continue;
+
+                diggerSystem.ModifyWithoutMeshes(operation);
+            }
+        }
+    }
+}
